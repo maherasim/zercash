e29235e06b4209c0dc3001d66a680dcf41850ef9
maherasim <asimriazasim107@gmail.com>
2026-02-28 22:50:38 +0500

    supplier registration screens

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -1640,6 +1640,7 @@
         }
         window.stayOnline = stayOnline;
         window.logoutNow = logoutNow;
+        window.closeWelcomeModal = closeWelcomeModal;
         })();
     </script>
 </body>
